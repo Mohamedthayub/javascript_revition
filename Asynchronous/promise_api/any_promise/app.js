2d9f4a926a7843e4f30a85d47ab42d4cee4ccb1f
buyath <buyaththayub@gmail.com>
2025-01-31 16:03:28 +0000

    promise api done

---
+++ Asynchronous/promise_api/any_promise/app.js
@@ -0,0 +1,24 @@
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("p1 Success");
+    },3000)
+})
+const p2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("p2 Success");
+    },2000)
+})
+
+
+const p3 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("p3 Success");
+    },1000)
+})
+
+Promise.any([p1,p2,p3]).then((res) => {
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+})
