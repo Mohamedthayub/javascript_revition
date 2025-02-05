c491a842e07c1a2ca27ccfa2f7cf418bbdc251ff
buyath <buyaththayub@gmail.com>
2025-02-05 16:46:34 +0000

    async await donw

---
+++ Asynchronous/akshay_async_await/error_handling.js
@@ -0,0 +1,15 @@
+const URL = "https://api.github.com/users/Mohamedthayub";
+async function getData(){
+    try{
+        const data = await fetch(URL); // this await keyword ro resolve this promise
+    
+    const response =await data.json(); 
+    
+    console.log(response);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+getData();
+
