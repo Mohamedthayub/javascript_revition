e61f80f2fe1abc266fda184bdad601a14a31501c
buyath <buyaththayub@gmail.com>
2025-08-19 22:34:38 +0000

    this keyword episode done

--- this/this.js
+++ this/this.js
@@ -1,14 +1,45 @@
-"use strict";
-
-function x(){
+"use strict"
+function call(){
     console.log(this);
 }
-window.x(); 
+call(); // if you call this function  using strict mode the output will be undefined
+window.call();  // if you call the function using window object the output will be window object
 
-// this keyword depends on how you call the function 
-// if you call the function in strict mode the value would be undefined 
+const  student = {
+    name : "Mohamed",
+    printName:function() {
+        console.log(this); // the value of this keyword would be object
 
-// otherwise if you call the function in non strict the value would be window 
+    }
+}
 
-// and if you call the function in strict mode with  window.x() the value would be window
- 
+const student2 = {
+    name : "Thayub"
+} 
+
+student.printName.call(student2);
+
+
+// this inside nested arrow function  
+
+const number = {
+    a : 20, 
+    x : function(){
+        // enclosing  lexical context 
+        const y = () => {
+            console.log(this)
+        }
+        y();
+    }
+    
+}
+number.x();
+
+// this inside arrow function  
+const obj = {
+    age : 19,
+    fun : () => {
+        console.log(this);
+    }
+}
+obj.fun();
